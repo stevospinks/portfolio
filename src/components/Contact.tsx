3c70f4318477cc4ffebfdadd3ca3e1a90edaf5e4
stevospinks <github@stephenspinks.com>
2021-04-27 12:04:10 +0100

    Update contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { FaEnvelopeSquare, FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 import '../css/Contact.scss';
 import { EmptyProps, EmptyState } from '../interfaces/empty';
 
@@ -10,16 +11,20 @@ class Contact extends React.Component<EmptyProps, EmptyState> {
         <p>
           Contact Me:
           <br />
-          <a href='https://twitter.com/stevospinks/' target='_blank' rel='noopener noreferrer'>
-            Twitter
+          <a href='https://github.com/stevospinks' target='_blank' rel='noopener noreferrer'>
+            <FaGithubSquare className='icon' title='GitHub' />
           </a>
-          {' | '}
-          <a href='mailto:stephen@stephenspinks.com' target='_blank' rel='noopener noreferrer'>
-            Direct E-mail
-          </a>
-          {' | '}
+          &nbsp;
           <a href='https://www.linkedin.com/in/stephenspinks' target='_blank' rel='noopener noreferrer'>
-            LinkedIn
+            <FaLinkedin className='icon' title='LinkedIn' />
+          </a>
+          &nbsp;
+          <a href='https://twitter.com/stevospinks/' target='_blank' rel='noopener noreferrer'>
+            <FaTwitterSquare className='icon' title='Twitter' />
+          </a>
+          &nbsp;
+          <a href='mailto:stephen@stephenspinks.com' target='_blank' rel='noopener noreferrer'>
+            <FaEnvelopeSquare className='icon' title='Direct E-mail' />
           </a>
         </p>
       </div>
